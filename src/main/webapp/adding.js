e4fd0f0ac3a99d292d1b73fe8e5c6b435b2c5ea4
emnordi <emnordi@kth.se>
2018-02-07 13:35:09 +0100

    startingcookies

--- src/main/webapp/adding.js
+++ src/main/webapp/adding.js
@@ -1,15 +1,13 @@
 
-function showRegForm() {
-    document.getElementById("regger").style.display = '';
-}
-
-
 
 $(document).ready(function () {
     $('#registerForm').hide();
 
     $('#dropdownMenuButton').click(function (e) {
         $('#registerForm').show();
-        $(this).hide();
+    });
+    
+    $('#dropBack').click(function (e) {
+        $('#registerForm').hide();
     });
 });
