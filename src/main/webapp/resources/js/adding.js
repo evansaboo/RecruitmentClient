b291dd7548dab8baeea88d6f9904858fcb5b53ee
saboo <saboo@kth.se>
2018-03-14 19:18:03 +0100

    Update adding.js

--- src/main/webapp/resources/js/adding.js
+++ src/main/webapp/resources/js/adding.js
@@ -13,35 +13,6 @@ $(document).ready(function () {
     msgToUser = msgToUser[0];
     if(!(msgToUser === ""))
         notify(msgToUser, msgType);
-    
-   
 });
 
-function notify(msg, alertType) {
-
-    $.notify({
-        message: msg
-
-    }, {
-        element: 'body',
-        position: null,
-        type: alertType,
-        allow_dismiss: true,
-        newest_on_top: true,
-        placement: {
-            from: "top",
-            align: "right"
-        },
-        offset: 20,
-        spacing: 10,
-        z_index: 1031,
-        delay: 5000,
-        timer: 1000,
-        animate: {
-            enter: 'animated fadeInDown',
-            exit: 'animated fadeOutUp'
-        }
-    });
-
-}
 
