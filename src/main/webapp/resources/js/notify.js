c08e877da4aebd626a865207908b4da58720010d
saboo <saboo@kth.se>
2018-03-14 19:14:12 +0100

    Update notify.js

--- src/main/webapp/resources/js/notify.js
+++ src/main/webapp/resources/js/notify.js
@@ -1,1 +1,27 @@
+function notify(msg, alertType) {
 
+    $.notify({
+        message: msg
+
+    }, {
+        element: 'body',
+        position: null,
+        type: alertType,
+        allow_dismiss: true,
+        newest_on_top: true,
+        placement: {
+            from: "top",
+            align: "right"
+        },
+        offset: 20,
+        spacing: 10,
+        z_index: 1031,
+        delay: 5000,
+        timer: 1000,
+        animate: {
+            enter: 'animated fadeInDown',
+            exit: 'animated fadeOutUp'
+        }
+    });
+
+}
